71a1506258bc178a6483e7849ace9547e58fba29
KihaJeong1994 <jkha7371@gmail.com>
2023-06-16 08:57:47 +0900

    remove User-Authority cascade option

--- src/main/java/com/example/springsecuritytemplate/domain/user/entity/User.java
+++ src/main/java/com/example/springsecuritytemplate/domain/user/entity/User.java
@@ -19,7 +19,7 @@ public class User {
 
     private String phone;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne
     @Setter
     private Authority authority;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,21 @@ INSERT INTO scope(name) values ('HELLO.READ');
 INSERT INTO scope(name) values ('HELLO.WRITE');
 INSERT INTO scope(name) values ('BYE.READ');
 INSERT INTO scope(name) values ('BYE.WRITE');
+
+INSERT INTO authority(name) values ('ADMIN');
+INSERT INTO authority(name) values ('WRITE');
+INSERT INTO authority(name) values ('READ');
+
+INSERT INTO authority_scope values (1,1);
+INSERT INTO authority_scope values (1,2);
+INSERT INTO authority_scope values (1,3);
+INSERT INTO authority_scope values (1,4);
+
+
+INSERT INTO authority_scope values (2,2);
+INSERT INTO authority_scope values (2,4);
+
+
+INSERT INTO authority_scope values (3,1);
+INSERT INTO authority_scope values (3,3);
+
